8163107aaaa5b71d07f5c5361858f2491b1128d3
Syrym Kosymbayev - Kultaev <79658480+Syrym-Kosymbayev-Kultaev@users.noreply.github.com>
2022-07-26 12:36:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
+addEls = [
+    'test',
+    'test2',
+    'test3',
+    'test4',
+]
+
+template = ''
+addEls.forEach(function(item, i) {
+    i++
+    temp = `
+    <li>
+        <input type="checkbox" id="${i}" onclick="fun(this)">
+        <label for="${i}">${item}</label>
+    </li>
+    `
+
+    template += temp
+});
+document.querySelector('.add').innerHTML = template
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    for(var item in localStorage)
+    {
+        if(item.length <= 2)
+        {
+            document.getElementById(`${item}`).click()
+        }
+    }
+
+})
+
 function fun(t) {
-    console.log(t.checked)
     if ( t.checked == true )
     {
+        localStorage.setItem(t.id, true);
         t.parentNode.classList.add("checked");
     }
     else
     {
+        localStorage.removeItem(t.id);
         t.parentNode.classList.remove("checked");
     }
 }
+
+document.querySelector('button').addEventListener('click', function() {
+    const result = confirm('Очистить все?');
+    if (result) {
+        for(var item in localStorage)
+        {
+            if(item.length <= 2)
+            {
+                document.getElementById(`${item}`).click()
+            }
+        }
+    }
+})
